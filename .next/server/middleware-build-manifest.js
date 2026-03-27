21f4e9be4a46e1b30468e745a67fb8b6a1a56283
CristianR8 <cristiancamiloreyrueda@gmail.com>
2026-03-27 08:45:12 -0500

    loading state fixed

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -28,6 +28,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/dip.js"
+    ],
+    "/statistics": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/statistics.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc752aba8c3c6bbd"; }
+/******/ 		__webpack_require__.h = function() { return "94c9247839360bd9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
